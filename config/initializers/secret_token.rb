1157fae6028f50cd22906979cd495711800b0d08
bfg <bfg@misha.(none)>
2012-05-14 13:19:50 +0400

    Начало разработки | Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rukrug::Application.config.secret_token = '24cbe09a31e89ab0a24a3c8126cf730230e194a8dfd26685ec4da12b98f1e429cf33bac51b953352624fc0c893239f3b203da0b03fd045ada2646e685348cd1c'
